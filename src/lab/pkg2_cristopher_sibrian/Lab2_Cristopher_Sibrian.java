0020ac74958a2c3fbe2286d1d6f855cbfb2da4d5
Cris147 <cristopher20061312@gmail.com>
2024-10-18 15:37:31 -0600

    Segundo commit

--- src/lab/pkg2_cristopher_sibrian/Lab2_Cristopher_Sibrian.java
+++ src/lab/pkg2_cristopher_sibrian/Lab2_Cristopher_Sibrian.java
@@ -4,6 +4,7 @@
  */
 package lab.pkg2_cristopher_sibrian;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -17,31 +18,55 @@ public class Lab2_Cristopher_Sibrian {
      */
     public static void main(String[] args) {
          Scanner lab = new Scanner(System.in);
-        int juego, num1, num2, pm, sm, tab, tab2;
+        int juego, num1, num2, p, s, tab, tab2;
         do{
              System.out.println("Menu de opciones \n1. Numero comunes en multiplicaion \n2. juego de suerte \n3. llenar el trapecio \n4. salida \nElija una opcion:");
         juego = lab.nextInt();
-        if (juego == 1)
+        if (juego == 1){
                 System.out.println("Ingrese el numero de la primera tabla:");
                 num1 = lab.nextInt();
                 System.out.println("Ingrese el numero de la segunda tabla: ");
                 num2 = lab.nextInt();
                 
-                pm = 1;
-                while(pm!=13){
-                    sm = 1;
-                    tab = num1 * pm;
-                    pm++;
-                    while (sm!=13){
-                        tab2 = num2 * sm;
-                        sm++;
+                p = 1;
+                while(p<13){
+                    tab = num1 * p;
+                    s = 1;
+                  
+                    while (s<13){
+                        tab2 = num2 * s;
                         if (tab == tab2){
-                            System.out.print(tab+" ");
+                            System.out.print(tab + " ");
                         }
-                        sm++;
+                        s++;
                     }
-                    System.out.print("_ "); 
+                    p++;
+                System.out.print("_ "); 
                 }
+        }
+        else if (juego == 2){
+               Random numeros = new Random();
+               int Pjugador = 0, Pmaquina = 0, ronda = 1;
+               boolean Gameover = false;
+               
+               while (ronda<= 3){
+                   System.out.println("\n Ronda " + ronda + " ");
+                   Pjugador += numeros.nextInt(10) + 1;
+                   
+                   while (!Gameover){
+                       System.out.println("Puntos del jugador "+ Pjugador);
+                       System.out.println("OPCIONES: \n1. Agregar 1 \n2.Agregar 2 \n3. Seguir \n Terminar");
+                   }
+                       
+                   
+                   
+               
+               }
+                 
+                       
+        }
+                       
+                
                       
                 
         }
